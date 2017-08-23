ab3a2d1f7b506f2ea20ceada6dfd3033f558e51b
Simon Shields <simon@lineageos.org>
2017-08-23 12:05:33 +1000

    soong: extend with custom board flags
    Includes the following commit:
    
    Author: Simon Shields <simon@lineageos.org>
    Date:   Mon Sep 4 21:09:44 2017 +1000
    
        soong: explicitly specify name of Lineage variables struct
    
        Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15
    
    Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3

--- android/variable.go
+++ android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"weeb/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -131,6 +133,9 @@ type variableProperties struct {
 			Srcs         []string `android:"arch_variant"`
 			Exclude_srcs []string `android:"arch_variant"`
 		} `android:"arch_variant"`
+
+		// include Weeb variables
+		Weeb android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -336,6 +341,9 @@ type productVariables struct {
 	InstallExtraFlattenedApexes *bool `json:",omitempty"`
 
 	BoardUsesRecoveryAsBoot *bool `json:",omitempty"`
+
+	// include Weeb variables
+	Weeb android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
